bc5d88e8cf41557fb4b77bcf72976138d2d23d39
RbYudha <youdha900.yr@gmail.com>
2020-04-03 14:23:12 +0700

    Added Fire Animation
    Menambahkan animasi api pada bola

--- Assets/ballScript.cs
+++ Assets/ballScript.cs
@@ -12,14 +12,26 @@ public class ballScript : MonoBehaviour
 
     public Rigidbody2D ballObj;
 
+    //public Animation fire;
+
+    public Animator fireAnim;
+
     void Start()
     {
         ballObj.velocity = new Vector2(1, -1) * speed;
+        fireAnim.SetBool("ballMoving", true);
     }
 
-    void Update()
+    void FixedUpdate()
     {
-        
+        //ball move to the right
+        if ( ballObj.velocity.x > 0 ) {
+            ballObj.GetComponent<Transform>().localScale = new Vector3 (1,1,1);
+
+        //ball move to the left
+        } else {
+            ballObj.GetComponent<Transform>().localScale = new Vector3 (-1,1,1);
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -32,9 +44,11 @@ public class ballScript : MonoBehaviour
     public IEnumerator ballRespawn()
      {
          ballObj.velocity = Vector2.zero;
+         fireAnim.SetBool("ballMoving", false);
          ballObj.GetComponent<Transform>().position = Vector2.zero;
          yield return new WaitForSeconds(ballDelay);
          ballObj.velocity = new Vector2(1, -1) * speed;
+         fireAnim.SetBool("ballMoving",false);
      }
 
 }
