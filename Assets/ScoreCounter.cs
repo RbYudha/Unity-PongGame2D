2c29b30e15759723d6029f4b35cddb950f945684
RbYudha <youdha900.yr@gmail.com>
2020-04-15 08:47:08 +0700

    update

---
+++ Assets/ScoreCounter.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreCounter : MonoBehaviour
+{
+    public Text scoreBoard;
+    public GameObject ball;
+
+    //private int player1Score = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ball = GameObject.Find ("Ball");
+    }
+
+    // void Update()
+    // {
+    //     if(ball.transform.position.x >= 27.5f){
+    //         player1Score ++;
+    //     }
+
+    //     print(player1Score);
+    // }
+
+}
